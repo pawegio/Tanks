1eb1b2490b745bf2f04ff1d7db16e6af8c9bfd3f
Paweł Gajda <pawegio@gmail.com>
2014-12-05 22:17:19 +0100

    empty project

---
+++ desktop/src/com/connectmedica/tanks/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.connectmedica.tanks.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.connectmedica.tanks.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
